d4d29fb5a6a98801af78c8faeff578ade5494082
faisala <alquaddoomi@nexus.ethz.ch>
2018-11-30 14:11:23 +0100

    Harvester entrypoint is now easier to configure for different target silos.

--- harvester/entrypoint.sh
+++ harvester/entrypoint.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
-# echo "POSTGRES_PASSWORD: $POSTGRES_PASSWORD"
-# echo "POSTGRES_DB: $POSTGRES_DB"
-# echo "POSTGRES_HOST: $POSTGRES_HOST"
-# echo "POSTGRES_USER: $POSTGRES_USER"
+declare -A SILOS
+SILOS[postgres]="--silos postgres --pg_host ${POSTGRES_HOST} --pg_db ${POSTGRES_DB} --pg_user ${POSTGRES_USER} --pg_pass ${POSTGRES_PASSWORD}"
+SILOS[elastic]="--silos elastic --elastic_search elasticsearch"
+SILOS[file]="--silos file --file_output_dir=/output/raw"
 
-# wait for postgres to wake up
-until PGPASSWORD="$POSTGRES_PASSWORD" psql -d  "$POSTGRES_DB" -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -c '\q' 2> /dev/null; do
-    >&2 echo "Postgres is unavailable at $POSTGRES_HOST - sleeping"
-    sleep 1
-done
+CHOSEN_SILO="postgres"
+
+if [[ ${CHOSEN_SILO} == "postgres" ]]; then
+    # wait for postgres to wake up
+    until PGPASSWORD="$POSTGRES_PASSWORD" psql -d  "$POSTGRES_DB" -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -c '\q' 2> /dev/null; do
+        >&2 echo "Postgres is unavailable at $POSTGRES_HOST - sleeping"
+        sleep 1
+    done
+fi
 
 # and do a test harvest, dumping the results to a file
-python harvester.py -ds --silos file --file_output_dir=/app/output_new \
-  --harvesters oncokb civic jax --genes BRAF --phases all
+python harvester.py -d \
+  ${SILOS[$CHOSEN_SILO]} \
+  --harvesters oncokb civic --genes BRAF
+  # --harvesters oncokb civic jax --genes BRAF --phases all
