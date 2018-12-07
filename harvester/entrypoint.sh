948ef8815df9b26e25ec31d52f4673d90f0526b0
faisala <alquaddoomi@nexus.ethz.ch>
2018-12-07 14:05:49 +0100

    Changed sample entrypoint to download two genes, BRAF and EGFR. Fixed shebang in harvester.py, added some missing requirements.

--- harvester/entrypoint.sh
+++ harvester/entrypoint.sh
@@ -18,5 +18,5 @@ fi
 # and do a test harvest, dumping the results to a file
 python harvester.py -d \
   ${SILOS[$CHOSEN_SILO]} \
-  --harvesters oncokb civic --genes BRAF
+  --harvesters oncokb civic --genes BRAF EGFR
   # --harvesters oncokb civic jax --genes BRAF --phases all
